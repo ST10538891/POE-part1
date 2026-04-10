9f9e10c01af57443ea43845ca36bbc1325887bb5
ST10538891 <ST10538891@rcconnect.edu.za>
2026-04-10 21:56:53 +0200

    Update Login.java
    Updated Login.java with clean implementation

--- src/main/java/com/mycompany/poe/Login.java
+++ src/main/java/com/mycompany/poe/Login.java
@@ -1,8 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.poe;
+ */package com.mycompany.poe;
 
 /**
  *
@@ -89,5 +88,6 @@ public class Login {
 
 }
     
+
 
 
